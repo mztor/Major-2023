81983a25f723bfd058e247eecaedc6629b29fd96
mztor <stor@macarthur.nsw.edu.au>
2023-03-03 10:46:30 +1100

    Initial Commit

---
+++ Major 2023/Major_2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Major_2023App.swift
+//  Major 2023
+//
+//  Created by Sarah Tor on 3/3/2023.
+//
+
+import SwiftUI
+
+@main
+struct Major_2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Major 2023UITests/Major_2023UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Major_2023UITestsLaunchTests.swift
+//  Major 2023UITests
+//
+//  Created by Sarah Tor on 3/3/2023.
+//
+
+import XCTest
+
+final class Major_2023UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
